9a95477d9fbcc08c227d8deb7208d63285c5bafb
Derek Cooper <derekcooper20@gmail.com>
2023-07-25 15:12:34 -0600

    Navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useMemo, useState } from "react"
+import { ThemeProvider, createTheme } from "@mui/material/styles"
+import { themeSettings } from "./theme"
+import { Box, CssBaseline } from "@mui/material"
+// import { useTheme } from "@mui/material"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import FrontPage from "./scenes/frontPage"
+import Navbar from "./components/navbar"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const theme = useMemo(() => createTheme(themeSettings), [])
+  // const { palette } = useTheme()
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="app">
+        <BrowserRouter>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+              <Navbar />
+              <Routes>
+              <Route path="/" element={<FrontPage />} />
+              </Routes>
+            </Box>
+          </ThemeProvider>
+        </BrowserRouter>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
